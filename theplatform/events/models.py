04ba6ba4f347d92bda37623577ecec98ff71490f
Ahmad Faiyaz <ahmad.faiyaz@newscred.com>
2017-06-26 02:26:20 +0600

    Add dynamic event menu

--- theplatform/events/models.py
+++ theplatform/events/models.py
@@ -1,16 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import django.utils.timezone
 from modelcluster.fields import ParentalKey
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch import index
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel, InlinePanel, StreamFieldPanel)
 
 from wagtailsurveys import models as surveys_models
+from theplatform.blocks import GlobalStreamBlock
 
 
 class EventIndexPage(Page):
@@ -18,13 +19,42 @@ class EventIndexPage(Page):
         'EventPage'
     ]
 
+	parent_page_types = ['home.HomePage']
+	
+	def get_context(self, request):
+		context = super(EventIndexPage, self).get_context(request)
+		context['events'] = EventPage.objects.descendant_of(
+		    self).live().order_by(
+		    '-first_published_at')
+		return context
+
+	
+
+
 class EventPage(Page):
+	summary = models.TextField(
+        help_text='An event summary which will be shown on index page',
+        blank=True)
+	
+	description = RichTextField(blank=True)
+	
+	datetime_from = models.DateTimeField('Start datetime', default=django.utils.timezone.now, blank=True)
+	datetime_to = models.DateTimeField('End datetime', null=True, blank = True)
+
 	parent_page_types = [
         'EventIndexPage'
     ]
 	subpage_types = [
         'EventRegistrationPage'
     ]
+
+	content_panels = Page.content_panels + [
+		FieldPanel('summary'),
+		FieldPanel('description'),
+		FieldPanel('datetime_from'),
+		FieldPanel('datetime_to'),
+	]
+
 
 class EventRegistrationPage(surveys_models.AbstractSurvey):
 	parent_page_types = [
