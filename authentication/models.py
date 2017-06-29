59b40a0224ed48230b7e052d7e4849a9c729b95c
Arman Kamal <arman1790@gmail.com>
2017-06-29 21:38:58 +0600

    Feature: added index to User model

--- authentication/models.py
+++ authentication/models.py
@@ -6,13 +6,12 @@ from django.contrib.auth.models import (
 
 
 class User(AbstractUser):
-    email = models.CharField(max_length=255, unique=True)
+    email = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=31)
     last_name = models.CharField(max_length=31)
     address = models.CharField(max_length=255, default="")
     institution = models.CharField(max_length=255)
     role = models.CharField(max_length=15)
     tshirt_size = models.CharField(max_length=4, default="L")
-    division = models.CharField(max_length=10)
+    division = models.CharField(db_index=True, max_length=10)
     verified = models.BooleanField(default=True)
-    
